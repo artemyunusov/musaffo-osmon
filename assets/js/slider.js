4df6d0f71dd19f7a249d03ceb7921bddafaf6b32
artemyunusov <artemyunusov@gmail.com>
2021-11-08 01:19:23 +0500

    first commit

---
+++ assets/js/slider.js
@@ -0,0 +1,25 @@
+$('.services-slider').slick({
+  slidesToShow: 3,
+  arrows: false,
+  dots: false,
+  // responsive: [
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 3
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 1
+  //     }
+  //   }
+  // ]
+});
